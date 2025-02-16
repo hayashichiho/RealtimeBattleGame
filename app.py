06028d68cfcbbdc27aafae0d08a7248aa129cc8e
hayashichiho <22t0805b@student.gs.chiba-u.jp>
2025-02-16 22:11:28 +0900

    [fix]一部修正

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from ss2413 import app, db
 
-if __name__ == "__main__":
+with app.app_context():
     db.create_all()  # SQLiteのデータベースファイルを作成
-    app.run(
-        host="0.0.0.0", port=5000, debug=True
-    )  # ポート5000でFlaskアプリケーションを起動
+
+if __name__ == "__main__":
+    app.run(debug=True)
